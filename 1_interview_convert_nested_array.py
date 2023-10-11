ecf6aef2e4fb3e11ebb71b178448e6dd33544aa5
Альбина Гильмутдинова <inovaras@mail.ru>
2023-10-11 19:18:07 +0500

    1_interview: convert nested array

---
+++ 1_interview_convert_nested_array.py
@@ -0,0 +1,15 @@
+# написать функцию, которая выполнит перобразрвание переданного массива с вложенными массивами в плоский список
+
+def make_lst(l):
+    lst = []
+    for elem in l:
+        if isinstance(elem, list):
+            lst.extend(make_lst(elem))
+        else:
+            lst.append(elem)
+    return lst
+
+
+print(make_lst([[1], 2, 3, [7, 9]]))
+print(make_lst([[1], 2, 3, [7, [[9]]]]))
+print(make_lst([[1], 2, 3, [7, 9]]))
