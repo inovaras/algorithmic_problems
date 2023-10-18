5b80b99a600a285568e866feb7bac456ce1c29f8
Альбина Гильмутдинова <inovaras@mail.ru>
2023-10-18 15:50:31 +0500

    2_interview_sum function_that_returns_the_given_arguments

---
+++ 2_interview_sum function_that_returns_the_given_arguments.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+
+def sum(*args):
+    acc = []
+    result = 0
+
+    def inner(*args):
+        nonlocal result
+        if len(args) == 0:
+            return result
+        else:
+            for i in args:
+                acc.append(i)
+            result = reduce(lambda x, y: x+y, acc)
+            print(result)
+            return inner
+
+    return inner(*args)
+
+
+print('final sum:', sum(1, 0)(1, 1, 1)(1)(1, 1)())
